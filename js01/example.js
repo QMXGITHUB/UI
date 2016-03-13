addd010aff87ab721c38fac2e621cc4696e6900a
mxqiao <mxqiao@thoughtworks.com>
2016-03-13 20:16:31 +0800

    js variable declare or init.

---
+++ js01/example.js
@@ -0,0 +1,31 @@
+var my_mood = "happy";
+var your_mood = "sad";
+if (my_mood == your_mood) {
+	alert("We are in same mood.");
+};
+if (my_mood != your_mood) {
+	alert("We are in different mood.");
+};
+
+var array = Array();
+array[0] = "first";
+array[1] = "second";
+
+array = ["first", "second"];
+
+array = Array(2);
+
+array = Array("first", 2, "third");
+
+var num = 33.25;
+var temperature = -20;
+var sleeping = true;
+
+var height = "about 5'10\" tall";
+alert(height);
+
+
+var mood = "happy";
+var age = 33;
+alert(mood);
+alert(age);
