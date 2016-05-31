fbcc8515685c8612ed73c970d6b58f76c25e51b1
mxqiao <mxqiao@thoughtworks.com>
2016-05-31 23:17:26 +0800

    arrraycontain, objectcontain

--- LearnJasmine/spec/basic.js
+++ LearnJasmine/spec/basic.js
@@ -418,3 +418,64 @@ describe("match any", function() {
   });
 });
 
+describe("jasmine.objectContaining", function() {
+  var foo;
+
+  beforeEach(function() {
+    foo = {
+      a: 1,
+      b: 2,
+      bar: "baz"
+    };
+  });
+
+  it("matches objects with the expect key/value pairs", function() {
+    expect(foo).toEqual(jasmine.objectContaining({
+      bar: "baz"
+    }));
+    expect(foo).not.toEqual(jasmine.objectContaining({
+      c: 37
+    }));
+  });
+
+  describe("when used with a spy", function() {
+    it("is useful for comparing arguments", function() {
+      var callback = jasmine.createSpy('callback');
+
+      callback({
+        bar: "baz"
+      });
+
+      expect(callback).toHaveBeenCalledWith(jasmine.objectContaining({
+        bar: "baz"
+      }));
+      expect(callback).not.toHaveBeenCalledWith(jasmine.objectContaining({
+        c: 37
+      }));
+    });
+  });
+});
+
+describe("jasmine.arrayContaining", function() {
+  var foo;
+
+  beforeEach(function() {
+    foo = [1, 2, 3, 4];
+  });
+
+  it("matches arrays with some of the values", function() {
+    expect(foo).toEqual(jasmine.arrayContaining([3, 1]));
+    expect(foo).not.toEqual(jasmine.arrayContaining([6]));
+  });
+
+  describe("when used with a spy", function() {
+    it("is useful when comparing arguments", function() {
+      var callback = jasmine.createSpy('callback');
+
+      callback([1, 2, 3, 4]);
+
+      expect(callback).toHaveBeenCalledWith(jasmine.arrayContaining([4, 2, 3]));
+      expect(callback).not.toHaveBeenCalledWith(jasmine.arrayContaining([5, 2]));
+    });
+  });
+});
